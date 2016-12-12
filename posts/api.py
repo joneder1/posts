0604b8928c73ea1ca53777c6f0c0d9830d298407
joneder1 <eder.jon@gmail.com>
2016-12-12 23:31:33 +0000

    Put Request for Edit Title/Body of Post

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,70 @@ from . import decorators
 from posts import app
 from .database import session
 
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
+
+
+#PUT Uploads a representation of the specified URI                    
+@app.route("/api/posts/<id>", methods=["PUT"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def post_edit(id):
+    """ Edit post """
+    data = request.json
+    
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+        
+    # edit post for specified id # by retrieving id
+    post = session.query(models.Post).get(id)
+    post.title = data["title"]
+    post.body = data["body"]
+    session.commit()
+
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 200, headers=headers,
+                    mimetype="application/json")                    
+
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -16,7 +80,6 @@ def posts_get():
     title_like = request.args.get("title_like")
     body_like = request.args.get("body_like")
     
-
     # Get and filter the posts from the database
     posts = session.query(models.Post)
     if title_like:
